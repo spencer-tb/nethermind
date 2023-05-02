c1906e964378ae022c51b02a891b29f34d8759ea
spencer-tb <spencer@spencertaylorbrown.uk>
2023-05-02 17:43:01 -0600

    Hive tst.

--- src/Nethermind/Nethermind.Blockchain/GenesisLoader.cs
+++ src/Nethermind/Nethermind.Blockchain/GenesisLoader.cs
@@ -58,8 +58,8 @@ namespace Nethermind.Blockchain
 
             genesis.Header.StateRoot = _stateProvider.StateRoot;
             genesis.Header.Hash = genesis.Header.CalculateHash();
-
-            _logger.Warn($"Genesis ({genesis.Header.Hash})");
+            _logger.Warn($"Genesis Header ({genesis.Header})");
+            _logger.Warn($"Genesis Hash ({genesis.Header.Hash})");
             return genesis;
         }
 
