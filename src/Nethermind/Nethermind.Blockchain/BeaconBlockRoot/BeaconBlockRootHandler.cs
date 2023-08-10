188f485950184e0dbfd7a5630bcd90a335c0b0c6
spencer-tb <spencer@spencertaylorbrown.uk>
2023-08-10 16:48:06 +0100

    BeaconBlockRootHandler: Commit state change after updating storage.

--- src/Nethermind/Nethermind.Blockchain/BeaconBlockRoot/BeaconBlockRootHandler.cs
+++ src/Nethermind/Nethermind.Blockchain/BeaconBlockRoot/BeaconBlockRootHandler.cs
@@ -28,5 +28,8 @@ public class BeaconBlockRootHandler : IBeaconBlockRootHandler
 
         stateProvider.Set(tsStorageCell, timestamp.ToBigEndian());
         stateProvider.Set(brStorageCell, parentBeaconBlockRoot.Bytes.ToArray());
+
+        stateProvider.Commit(spec);
+        stateProvider.RecalculateStateRoot();
     }
 }
